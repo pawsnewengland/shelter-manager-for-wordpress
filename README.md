e23a7fd174271921907ca1003e1a0d513fcade6e
Chris Ferdinandi <hello@gomakethings.com>
2015-02-10 21:14:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Shelter Manager for WordPress
+
+Integrates Shelter Manager with WordPress.
+
+
+## Changelog
+
+* v1.1.0 - February 10, 2015
+	* Updated login credentials.
+* v1.0.0 - February 9, 2015
+	* Initial release.
---
+++ shelter-manager-for-wp.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Plugin Name: Shelter Manager for WordPress
+ * Plugin URI: https://github.com/pawsnewengland/shelter-manager-for-wordpress
+ * Description: Shelther Manager integration with WordPress.
+ * Version: 1.1.0
+ * Author: Chris Ferdinandi
+ * Author URI: http://gomakethings.com
+ * License: MIT
+ */
+
+function sm_get_pet_list($atts) {
+
+	// Extract shortcode values
+	extract(shortcode_atts(array(
+		'shelter_id' => '',
+		'username' => '',
+		'password' => ''
+	), $atts));
+
+	// End if variables not set
+	if ( empty($shelter_id) || empty($username) || empty($password) ) { return; }
+
+	// Get data from server
+	$url = 'http://sheltermanager.com/asm/service?account=' . $shelter_id . '&method=xml_adoptable_animals&username=' . $username . '&password=' . $password;
+	$xml = @simplexml_load_file( $url );
+	$data = json_decode(json_encode($xml), true);
+
+	// Create select elements
+	$options = '';
+	foreach ( $data[row] as $pet ) {
+		$options .= '<option value="' . $pet[animalname] . '">' . $pet[animalname] . ' (' . $pet[sheltercode] . ')</option>';
+	}
+
+	return $options;
+
+}
+add_shortcode('sm_get_pet_list', 'sm_get_pet_list');
